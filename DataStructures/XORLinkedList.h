4c9e2ee6976758b4a389c885e48dbef7cd1c66e4
m4ytaRo <maytarosheejsh@gmail.com>
2025-04-05 12:00:28 +0300

    Fixing some issues + external implementations (ostream operator, print, push_back) getXorProduct is const now Node constructor updated List constructor updated

--- DataStructures/XORLinkedList.h
+++ DataStructures/XORLinkedList.h
@@ -11,17 +11,17 @@ private:
     struct Node {
         T data;
         Node* xorProduct;
-        Node(const T& data, Node* xorProduct = nullptr) {};
+        Node(const T& data, Node* xorProduct = nullptr) : data(data), xorProduct(xorProduct) {};
     };
 
     template <class U>
     friend std::ostream& operator << (std::ostream& out, const XORLinkedList<U>& list);
 
-    Node* getXorProduct(Node* a, Node* b);
+    Node* getXorProduct(Node* a, Node* b) const;
     Node* head;
     Node* tail;
 public:
-    XORLinkedList() : head(nullptr) {};
+    XORLinkedList() : head(nullptr), tail(nullptr) {};
     XORLinkedList(XORLinkedList&& list) = delete;
     XORLinkedList(const XORLinkedList& list) = delete;
     XORLinkedList& operator=(XORLinkedList&& list) = delete;
@@ -32,13 +32,14 @@ public:
     void push_front(const T& data);
     void pop_back();
     void pop_front();
+    void remove(const T& data, int number);
     T& front();
     T& back();
-    void remove(const T& data, int number);
+    void print(std::ostream& out) const;
 };
 
 template <class T>
-typename XORLinkedList<T>::Node* XORLinkedList<T>::getXorProduct(typename XORLinkedList<T>::Node* a, typename XORLinkedList<T>::Node* b) {
+typename XORLinkedList<T>::Node* XORLinkedList<T>::getXorProduct(typename XORLinkedList<T>::Node* a, typename XORLinkedList<T>::Node* b) const{
     return reinterpret_cast<Node*> (reinterpret_cast<uintptr_t> (a) ^ reinterpret_cast<uintptr_t> (b));
     /*
     We cant do xor operations with Node pointer
@@ -54,7 +55,15 @@ bool XORLinkedList<T>::insert(const T& data, int positionIndex) {
 
 template <class T>
 void XORLinkedList<T>::push_back(const T& data) {
-
+    if (!head) {
+        head = tail = new Node(data, getXorProduct(nullptr, nullptr));
+    }
+    else {
+        Node* prevTail = tail;
+        Node* prevPrev = getXorProduct(prevTail->xorProduct, nullptr); // getting the adress of tail's prev element
+        tail = new Node(data, getXorProduct(nullptr, prevTail));
+        prevTail->xorProduct = getXorProduct(prevPrev, tail);
+    }
 }
 
 template <class T>
@@ -88,7 +97,25 @@ void XORLinkedList<T>::remove(const T& data, int number) {
 }
 
 template <class T>
+void XORLinkedList<T>::print (std::ostream& out) const{
+    if (!head) {
+        out << "List is empty\n";
+        return;
+    }
+    Node* ptr = head;
+    Node* ptrPrev = nullptr;
+    while (ptr) {
+        out << ptr->data << ' ';
+        Node* next = getXorProduct(ptr->xorProduct, ptrPrev);
+        ptrPrev = ptr;
+        ptr = next;
+    }
+    out << '\n';
+}
+
+template <class T>
 std::ostream& operator << (std::ostream& out, const XORLinkedList<T>& list) {
-    
+    list.print(out);
+    return out;
 }
 #endif
--- DataStructures/main.cpp
+++ DataStructures/main.cpp
@@ -2,5 +2,10 @@
 #include "XORLinkedList.h"
 
 int main() {
+    XORLinkedList<int> list;
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    std::cout << list;
     return 0;
 }
