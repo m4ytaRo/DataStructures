2f106422f1789f819076405bf11bad6edb83168e
m4ytaRo <maytarosheejsh@gmail.com>
2025-04-18 21:00:46 +0300

    Some additions

--- DataStructures/HashSet.h
+++ DataStructures/HashSet.h
@@ -7,20 +7,27 @@ class HashSet
     static_assert(
         sizeof(Key) == 0, "Unsupported key type.\n"
         );
-
 };
 
 template<>
 class HashSet<double> {
 
 private:
-    size_t size;
+    
     struct Line {
-
+        double* key_ = nullptr;
+        bool deleted_ = false;
     };
+    size_t size_;
+    size_t number_;
+    Line* table_;
 
 public:
-    HashSet(size_t size = 10) : size(size) {
+    static int calculateHash(double value, double k) {
+        return value * k;
+    }
+    HashSet() : size_(100), table_(new Line[100]) {};
+    HashSet(size_t size) : size_(size), table_(new Line[size]) {
 
     }
 };
